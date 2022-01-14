a4fd64d66c332b97d3e5099a81af59869b1e4be9
Ruben Fernandez Sanchez <1173665@alu.murciaeduca.es>
2022-01-14 08:26:12 +0100

    Ejercicios 1

---
+++ database/migrations/2022_01_12_070951_create_centros_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCentrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('centros', function (Blueprint $table) {
+            $table->id();
+            $table->integer('codigo')->unsigned();
+            $table->string('nombre', 100);
+            $table->string('web', 255)->nullable();
+            $table->point('situacion')->nullable();
+            $table->bigInteger('coordinador')->unsigned()->nullable();
+            $table->boolean('verificado')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('centros');
+    }
+}
