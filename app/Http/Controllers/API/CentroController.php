e081ef925faa16ea184654ceef4f21aba6f88266
Alberto Sierra <alberto.sierra@murciaeduca.es>
2022-01-14 08:12:13 +0100

    Controlador de recursos del modelo Centro

---
+++ app/Http/Controllers/API/CentroController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Centro;
+use Illuminate\Http\Request;
+use App\Http\Resources\CentroResource;
+
+class CentroController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return CentroResource::collection(Centro::paginate());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $centro = json_decode($request->getContent(), true);
+
+        $centro = Centro::create($centro);
+
+        return new CentroResource($centro);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Centro  $centro
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Centro $centro)
+    {
+        return new CentroResource($centro);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Centro  $centro
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Centro $centro)
+    {
+        $centroData = json_decode($request->getContent(), true);
+        $centro->update($centroData);
+
+        return new CentroResource($centro);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Centro  $centro
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Centro $centro)
+    {
+        $centro->delete();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\API\CentroController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Psr\Http\Message\ServerRequestInterface;
+use Tqdev\PhpCrudApi\Api;
+use Tqdev\PhpCrudApi\Config;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +17,21 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::apiResource('centros', CentroController::class);
+
+Route::any('/{any}', function (ServerRequestInterface $request) {
+    $config = new Config([
+        'address' => env('DB_HOST', '127.0.0.1'),
+        'database' => env('DB_DATABASE', 'forge'),
+        'username' => env('DB_USERNAME', 'forge'),
+        'password' => env('DB_PASSWORD', ''),
+        'basePath' => '/api',
+    ]);
+    $api = new Api($config);
+    $response = $api->handle($request);
+    return $response;
+})->where('any', '.*');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
