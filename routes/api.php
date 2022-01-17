d18e80a2ee2bdc144abdbde0eacc54a3746f356e
Felipe <1848472@alu.murciaeduca.es>
2022-01-17 09:48:32 +0100

    Añadiendo el controlador de recursos de la tabla materias

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\API\CentroController;
 use App\Http\Controllers\API\NivelController;
+use App\Http\Controllers\API\MateriaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Psr\Http\Message\ServerRequestInterface;
@@ -25,6 +26,9 @@ Route::apiResource('niveles', NivelController::class)
 ->parameters([
     'niveles' => 'nivel'
 ]);
+
+Route::apiResource('materias', MateriaController::class);
+
 
 Route::any('/{any}', function (ServerRequestInterface $request) {
     $config = new Config([
