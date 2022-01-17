83ca5df6e62c57b3a161962388a5ca652ee43999
andres <andrescovg@gmail.com>
2022-01-17 18:27:27 +0100

    Controlador de recursos Grupos

---
+++ database/migrations/2022_01_17_080519_create_grupos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGruposTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('grupos', function (Blueprint $table) {
+            $table->id();
+            $table->integer('curso');
+            $table->string('letra');
+            $table->string('nombre');
+            $table->integer('tutor');
+            $table->integer('anyoescolar');
+            $table->integer('nivel');
+            $table->boolean('verificado');
+            $table->integer('creador');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('grupos');
+    }
+}
